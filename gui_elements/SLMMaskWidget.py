e3f15fec5be128e0d906fb810af238d0e7e46f50
ayto <arouxel@laas.fr>
2023-06-21 00:31:47 +0300

    update logger + add SLM masks options

--- gui_elements/SLMMaskWidget.py
+++ gui_elements/SLMMaskWidget.py
@@ -26,9 +26,9 @@ class MaskParamsWidget(QWidget):
         self.mask_type_selector.addItem("modulation amplitude")
         # self.mask_type_selector.addItem("Grating")
         # self.mask_type_selector.addItem("Rect Amplitude")
-        # self.mask_type_selector.addItem("Phase Jump")
-        # self.mask_type_selector.addItem("Phase Reversal")
-        # self.mask_type_selector.addItem("Weights Sinc")
+        self.mask_type_selector.addItem("Phase Jump")
+        self.mask_type_selector.addItem("Phase Reversal")
+        #self.mask_type_selector.addItem("Weights Sinc")
         self.mask_type_selector.addItem("Custom h5 Mask")
         self.mask_type_selector.currentIndexChanged.connect(self.update_mask_params)
 
